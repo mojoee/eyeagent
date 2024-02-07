6c8b85dac2a09d813194081e05198e4e4a7f880a
mojoee <47809126+mojoee@users.noreply.github.com>
2024-02-07 10:57:14 +0800

    created class for Database interactions

--- api/src/utils.py
+++ api/src/utils.py
@@ -4,27 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def getClient():
-    url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_KEY")
-    supabase: Client = create_client(url, key)
-    return supabase
-
-def readPostgreSQL():
-    supabase = getClient()
-
-    try:
-        # Connect to your Supabase database
-        response = supabase.table('Images').select("*").execute()
-        print(response)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    return response
+class DatabaseHandler():
+    def __init__(self) -> None:
+        self.url: str = os.environ.get("SUPABASE_URL")
+        self.key: str = os.environ.get("SUPABASE_KEY")
+        self.client: Client = create_client(self.url, self.key)
 
 
-def uploadImage(file, bucket, path_on_supastorage, content_type="image/jpeg"):
-    supabase = getClient()
-    supabase.storage.from_(bucket).upload(file=file, path=path_on_supastorage, file_options={"content-type": f"{content_type}" })
-    print("Image uploaded")
+    def readPostgreSQL(self):
+        try:
+            # Connect to your Supabase database
+            response = self.client.table('Images').select("*").execute()
+            print(response)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        return response
 
 
+    def uploadImage(self, file, bucket, path_on_supastorage, content_type="image/jpeg"):
+        self.client.storage.from_(bucket).upload(file=file, path=path_on_supastorage, file_options={"content-type": f"{content_type}" })
+        print("Image uploaded")
+
+
+    def createDatabaseEntryImage(self, table, user_id):
+        data, count = self.client.table(table).insert({"user_id": user_id}).execute()
+        return data, count

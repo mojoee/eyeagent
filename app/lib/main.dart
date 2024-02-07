5dcad3d1a412060280f97942d937e8a0fce0b10b
Tobias Meixner <meixner.tobias@gmail.com>
2024-02-07 22:06:43 +0800

    clean up

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,23 +1,17 @@
 import 'dart:async';
-import 'dart:io';
 
-import 'package:camera/camera.dart';
+// ignore: unused_import
 import 'package:camera_windows/camera_windows.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:eyeagent/screens/login_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:eyeagent/utils/constants.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:eyeagent/screens/splash_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://yerngrprgzhttyniubnk.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inllcm5ncnByZ3podHR5bml1Ym5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxOTIwNzQsImV4cCI6MjAyMjc2ODA3NH0.JMfB2OrYlEpnoInMdS4iUWru1as7j0qfXF18dFz2BvY',
-  );
+      url: dotenv.env['SUPABASE_URL']!, anonKey: dotenv.env['SUPABASE_KEY']!);
   runApp(const MyApp());
 }
 
@@ -37,48 +31,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-  //   /// Web Client ID that you registered with Google Cloud.
-  // const webClientId = '705656740849-egjh6eaotpmd57q0m75c9ccq6ujm8j5d.apps.googleusercontent.com';
-
-  //   /// TODO: update the iOS client ID with your own.
-  //   ///
-  //   /// iOS Client ID that you registered with Google Cloud.
-  // const iosClientId = '705656740849-fnvoufc1evrd7amuejct0usckl80fir2.apps.googleusercontent.com';
-
-// void main() async {
-//   /// TODO: update Supabase credentials with your own
-//   await Supabase.initialize(
-//     url: 'https://yerngrprgzhttyniubnk.supabase.co',
-//     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inllcm5ncnByZ3podHR5bml1Ym5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxOTIwNzQsImV4cCI6MjAyMjc2ODA3NH0.JMfB2OrYlEpnoInMdS4iUWru1as7j0qfXF18dFz2BvY',
-//   );
-//   runApp(const MyApp());
-// }
-
-// final supabase = Supabase.instance.client;
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Auth',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const LoginScreen(),
-//     );
-//   }
-// }
-
-  //   TakePictureScreen(
-    //       // Pass the appropriate camera to the TakePictureScreen widget.
-    //       camera: firstCamera,
-    //       supabase: supabase),
-    // ),
-
-
-// A screen that allows users to take a picture using a given camera.
